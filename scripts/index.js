89b9f5487bd46a1dec7b9ddb67031bedaad35fdb
debajit13 <mallickdebajit3@gmail.com>
2020-07-08 10:12:38 +0530

    Update project structure
    - Added css folder, add styles.css inside it.
    - Added scripts folder, add index.js inside it.
    - Added media folder, add 'screenshot.png' and 'logo.png' inside it.
    - Changed drum image paths in the styles.css to relative path.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 //Detecting mouse press
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     playMusic(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
@@ -8,44 +8,44 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 }
 
 //detecting keyboard presses
-document.addEventListener("keypress", function(event) {
+document.addEventListener("keypress", function (event) {
   playMusic(event.key);
   buttonAnimation(event.key);
 });
 
 function playMusic(key) {
   switch (key) {
-    case 'w':
+    case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
-    case 'a':
+    case "a":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
-    case 's':
+    case "s":
       var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
 
-    case 'd':
+    case "d":
       var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
 
-    case 'j':
+    case "j":
       var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
-    case 'k':
+    case "k":
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
 
-    case 'l':
+    case "l":
       var kickBass = new Audio("sounds/kick-bass.mp3");
       kickBass.play();
       break;
@@ -55,7 +55,7 @@ function playMusic(key) {
 function buttonAnimation(currentKey) {
   var activeKey = document.querySelector("." + currentKey);
   activeKey.classList.add("pressed");
-  setTimeout(function() {
+  setTimeout(function () {
     activeKey.classList.remove("pressed");
   }, 100);
 }
